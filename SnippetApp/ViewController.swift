be32af22dabb39ec7abb0c5c2224d58f669bc949
Joshua <Rambo@128-223-223-82-wireless.uoregon.edu>
2017-07-29 15:51:53 -0700

    updated ViewController

--- SnippetApp/ViewController.swift
+++ SnippetApp/ViewController.swift
@@ -29,11 +29,40 @@ class ViewController: UIViewController {
      *
      */
     @IBAction func createNewSnippet(_sender: AnyObject) {
-        //create a constant that will store the snippetData then add the snippetdata to the snippetData Array
+        //create the UI alert controllers to prompt the the user to select a type of snippet start with the constants
+        let alert = UIAlertController(title: "Select a snippet type", message: nil, preferredStyle: .actionSheet);
+        /*
+         Here the UIALertActions work more as an option menu for the user
+         *
+         Each alert has three things that have to be inside of it -- the "Title" the "Style" and the completion handler which is what it does when the button is clicked.
+         *
+         *
+         in the final parts of the UIAlertActions we are initiliazing and appending the SnippetData Struct to the Data Array
+         *
+         the "self" keyword makes it so that you are explicit about the scope and reuse it whenever the completion handler is run
+         */
         
-        let newSnippet = SnippetData(); /* The "SnippetData on the left hand side is referring to the Struct file we created earlier */
-        data.append(newSnippet);
+        //create the UIAlertAction for when the user chooses text
+        let textAction = UIAlertAction(title: "text", style: .default) { (alert: UIAlertAction!) -> Void in self.data.append(SnippetData(snippetType: .text))
+        }
+        
+        //create the UIAlertAction for when the user chooses a photo
+        let photoAction = UIAlertAction(title: "photo", style: .default) {(alert: UIAlertAction!) -> Void in self.data.append(SnippetData(snippetType: .photo))
+        }
+        
+        //create the UIALertAction for when the User chooses to cancel making a snippet 
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        //add the alert actions to the UIAlertController
+        /*
+         THe UIAlertController is the Window while the ALertACtions are the objects/Button within the window.
+         */
+        alert.addAction(textAction)
+        alert.addAction(photoAction)
+        alert.addAction(cancelAction)
+        
+        ///present the ViewController 
+        present(alert, animated: true, completion: nil);
     }
-
 }
 
